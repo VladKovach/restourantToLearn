7421e544602555ff0d0786c588c354344ec1ce67
Vladyslav <vladkovach00@gmail.com>
2024-09-15 10:57:03 +0200

    fix some style

--- src/components/SearchComponent/SearchComponent.jsx
+++ src/components/SearchComponent/SearchComponent.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './searchCompBtn.css'
 const SearchComponent = () => {
   return (
-    <div className="searchComponent">
-      <input placeholder="What do you want?" className="button-30" type="text" />
-      <button className="button-30">Search</button>
+    <div className="searchComponent ">
+      <input placeholder="What do you want?" className="button-30 light" type="text" />
+      <button className="button-30 light">Search</button>
     </div>
   )
 }
